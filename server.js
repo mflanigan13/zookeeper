5699684913ba1dff15c2c20b8c9ddf808c15e0d2
mflanigan <mflanigantwualumn@gmail.com>
2022-01-25 18:51:10 -0600

    finished basic setup of server

--- server.js
+++ server.js
@@ -34,6 +34,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 // route to data
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -41,6 +46,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {

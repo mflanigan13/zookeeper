9f0a486bbe2fc028dca58c43e698cc2f537089bc
mflanigan <mflanigantwualumn@gmail.com>
2022-01-25 18:38:11 -0600

    Add Heroku

---
+++ server.js
@@ -0,0 +1,50 @@
+// global constants
+const express = require('express');
+const { animals } = require('./data/animals');
+
+// set up the express server with port
+const app = express();
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    // return the filtered results:
+    return filteredResults;
+}
+
+// route to data
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
+
+
+

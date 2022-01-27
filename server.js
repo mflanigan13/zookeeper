2d1a0a644e062af5cda6b2a437453409575189fe
mflanigan <mflanigantwualumn@gmail.com>
2022-01-26 18:05:04 -0600

    implemented front end

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-// global constants
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const { animals } = require('./data/animals');
 
-// set up the express server with port
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -38,7 +34,6 @@ function filterByQuery(query, animalsArray) {
     if (query.name) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
-    // return the filtered results:
     return filteredResults;
 }
 
@@ -73,7 +68,6 @@ function validateAnimal(animal) {
     return true;
 }
 
-// route to data
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -95,7 +89,6 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
         res.status(400).send('The animal is not properly formatted.');
     } else {
@@ -104,9 +97,27 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+
 
 
 
